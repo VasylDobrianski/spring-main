e6988636025aa0128caf052592962a500b8fee04
Vasyl <vasyl.b.dobrianski@gmail.com>
2022-10-23 11:44:08 -0500

    Table + Data insert test (2)

--- spring-11-Data-TableCreation/src/main/resources/data.sql
+++ spring-11-Data-TableCreation/src/main/resources/data.sql
@@ -1,2 +1,8 @@
 insert into EMPLOYEES(id,name)
 values (1,'Mike');
+
+insert into students(id,first_name,last_name,email)
+values (1,'Mike','Smith','mike@cydeo.com');
+
+insert into students(id,first_name,last_name,email)
+values (2,'Tom','Hanks','tom@cydeo.com');
